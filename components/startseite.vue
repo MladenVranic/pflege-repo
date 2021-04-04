ed6b752f274591dfb5ee8f7fb8b8fc720848b36a
Mladen <mladen.vrn@gmail.com>
2021-04-04 23:48:19 +0200

    Balken hinzugefügt & Kommentare aktualisiert

--- components/startseite.vue
+++ components/startseite.vue
@@ -10,40 +10,40 @@
             <div class="intro-form" >
                 <!--Überschrift-->
                 <div class="titel">
-                    <h2 v-scrollanimation>. . . Ihr Pflegedienst in Frankfurt und Umgebung</h2>
+                    <h2 v-scrollanimation>Sirius Pflegedienst in Frankfurt und Umgebung</h2>
                 </div>
                 <!--Text & Icons in der Form-->
-                    <div class="form-text" v-scrollanimation>
-                        <div><img src="../assets/starIcon.png" alt="" class="icon"></div>
-                            <div>
-                                <p>
-                                    <strong>
-                                        langjährige Erfahrung
-                                    </strong>
-                                </p>
-                            </div>
+                <div class="form-text" v-scrollanimation>
+                    <div><img src="../assets/starIcon.png" alt="" class="icon"></div>
+                        <div>
+                            <p>
+                                <strong>
+                                    langjährige Erfahrung
+                                </strong>
+                            </p>
+                        </div>
 
-                    <div class="form-text-2">
-                        <div> <img src="../assets/starIcon.png" alt="" class="icon"></div>
-                            <div>
-                                <p>
-                                    <strong>
-                                        24h verfügbar
-                                    </strong>
-                                </p>
-                            </div>
+                <div class="form-text-2">
+                    <div> <img src="../assets/starIcon.png" alt="" class="icon"></div>
+                        <div>
+                            <p>
+                                <strong>
+                                    24h verfügbar
+                                </strong>
+                            </p>
                         </div>
-                        <div class="form-text-3">
-                            <div>
-                                <p>
-                                    <strong>
-                                        sirius.pd@outlook.de
-                                    </strong>
-                                </p>
-                            </div>
+                    </div>
+                    <div class="form-text-3">
+                        <div>
+                            <p>
+                                <strong>
+                                    sirius.pd@outlook.de
+                                </strong>
+                            </p>
                         </div>
-                </div>
-            </div>
+                    </div><!--Ende Form-text-3-->
+                </div><!--Ende Form-text-->
+            </div><!--Ende Intro Form-->
         </div>
 
 <div class="inhalt">
@@ -64,11 +64,13 @@
                 </p>
                     <div class="intro-list" v-scrollanimation>
                         <ul><li><span>Grundpflege</span></li></ul>
+                        <ul><li><span>Behandlungspflege</span></li></ul>
                         <ul><li><span>Intensivpflege</span></li></ul>
                         <ul><li><span>Serviceleistungen</span></li></ul>
                     </div>
                 </div>
-                <br>
+              
+
                 <div class="txt">
                     <p v-scrollanimation>
                         <strong>
@@ -79,93 +81,143 @@
                         </strong>
                     </p>
                 </div>
-            </div>
-        <br>
-        <br>
+            </div><!--Ende Intro Text-->
+        </div><!--Ende Inhalt--> 
+
+        <!--GrundPflege Überschrift-->
             <div class="grundpflege" v-scrollanimation>
                 <div><img src="../assets/grundpflegeIcon.png" alt=""></div>
-                <div><h2>Grundpflege:</h2></div>
-            </div>
-        <br>
-            <div class="grundpflege-text">
-                <div class="txt">
-                    <p v-scrollanimation>
-                    <strong>
-                        Menschen, die körperlich und geistig nicht mehr so fit sind,
-                        können meistens alltägliche Tätigkeiten, wie z.B selbständig essen,
-                        das Klo aufsuchen und sich selbständig anziehen nicht mehr bewältigen.
-                        Folgende Leistungen zählen zu der Grundpflege:
-                        <br><br>
-                            <ul> <li><span>Körperpflege</span></li></ul>
-                            <ul> <li><span>Hilfe beim An - und Ausziehen</span></li></ul>
-                            <ul> <li><span>Hilfe beim Essen und Trinken</span></li> </ul>
-                            <ul> <li><span>Bewegungshilfe</span></li></ul>
-                            <ul><li><span>Hauswirtschaftliche Aufgaben</span></li></ul>
-                    </strong>
-                </p>
-            </div>
-            <br>
-            <div class="txt">
-            <p v-scrollanimation>
-                <strong>
-                    Natürlich fällt auch die <span>Vorsorge</span> in die Grundpflege.
-                    Durch die <span>Vorsorge</span> werden Einschränkungen wie Erkältung,
-                    Versteifung und weitere Beschwerden vermieden. Um eine sorgfältige
-                    Vorsorge durchzuführen, kümmern wir uns um jegliche negative Einflüsse
-                    unserer Patienten und beseitigen diese.
-                </strong>
-            </p>
-            </div>
-        </div>
-        <br>
-        <div class="intensiv-pflege" v-scrollanimation>
-            <div><img src="../assets/intensivIcon.png" alt=""></div>
-            <div><h2>Intensivpflege:</h2></div>
-        </div>
-        <br>
-        <div class="intensivpflege-text">
-            <div class="txt">
-            <p v-scrollanimation>
-                <strong>
-                    Die Intensivpflege ist ein wesentlicher Bestandteil unserer Leistungen.
-                    Ambulante Intensivpflege bezeichnet die Behandlung pflegebedürftiger Menschen
-                    mit lebensbedrohlicher Erkrankungen außerhalb der Klinik.
-                    Sie tritt in kraft, wenn eine stationäre klinische Behandlung und 
-                    medizinischer Versorgung nicht mehr erforderlich ist. 
-                </strong>
-            </p>
-            </div>
-           
-            <br>
-            <div class="txt">
-            <p v-scrollanimation>
-                <strong>
-                    Folgende Leistungen
-                    bieten wir für die Intensivpflege an: <br>
-                    <div class="intensiv-list">
-                    <ul><li><span>Überwachung & Vitalisierung</span></li></ul>
-                    <ul><li><span>Lagerungsbehandlungen & Mobilisation</span></li></ul>
+                    <div><h2>Grundpflege:</h2></div>
+                </div>
+            <!--Informationstext über die Grundpflege-->
+                <div class="grundpflege-text">
+                    <div class="txt">
+                        <p v-scrollanimation>
+                        <strong>
+                            Menschen, die körperlich und geistig nicht mehr so fit sind,
+                            können meistens alltägliche Tätigkeiten, wie z.B selbständig essen,
+                            das Klo aufsuchen und sich selbständig anziehen nicht mehr bewältigen.
+                            Natürlich fällt auch die <span>Vorsorge</span> in die Grundpflege.
+                            </strong>
+                            </p>
+                        </div>   
+                        <div class="txt">
+                            <p v-scrollanimation>
+                                <strong>
+                                    Durch die <span>Vorsorge</span> werden Einschränkungen wie Erkältung,
+                                    Versteifung und weitere Beschwerden vermieden. Um eine sorgfältige
+                                    Vorsorge durchzuführen, kümmern wir uns um jegliche negative Einflüsse
+                                    unserer Patienten und beseitigen diese.
+                                </strong>
+                            </p>
+                        </div>
                     </div>
-                </strong>
-            </p>
+
+              <!--Informationskontainer für Intensivpflege infos. und service Leistungen-->   
+    <div class="intensiv-service-information">
+        <div>
+            <!--Intensivpflege überschrift-->
+            <div class="intensiv-pflege" v-scrollanimation>
+                <div><img src="../assets/intensivIcon.png" alt=""></div>
+                <div><h2>Intensivpflege:</h2></div>
             </div>
-        </div>
-        <br>
-        <div class="service-leistungen" v-scrollanimation>
-            <div><img src="../assets/serviceIcon.png" alt=""></div>
-            <div><h2>Serviceleistungen:</h2></div>
-        </div>
-        <div class="service-text">
-            <p v-scrollanimation>
-                <strong>
-                    Zu unseren Servieleistungen gehört unter anderem die <span> häusliche
-                    Krankenpflege nach §37 SGB V  </span> als Leistung der gesetzlichen Krankenversicherung,
-                    welche die <span>Medikamentengabe</span>, den <span>Verbandswechsel</span>  und <span>Injektionen</span> beinhaltet.
-                </strong>
-            </p>
-        </div>
-    </div>
-</div>
+        <!--Informationstext über die Intensivpflege-->
+
+                <div class="intensivpflege-text">
+                    <div class="txt">
+                        <p v-scrollanimation>
+                            <strong>
+                                Die Intensivpflege ist ein wesentlicher Bestandteil unserer Leistungen.
+                                Ambulante Intensivpflege bezeichnet die Behandlung pflegebedürftiger Menschen
+                                mit lebensbedrohlicher Erkrankungen außerhalb der Klinik.
+                                Sie tritt in kraft, wenn eine stationäre klinische Behandlung und 
+                                medizinischer Versorgung nicht mehr erforderlich ist. 
+                            </strong>
+                        </p>
+                    </div>
+                </div>
+            </div><!--Ende Intensivpflege Kontainer-->
+                      <!--Service Leistungen Überschrift-->
+        <div>
+            <div class="service-leistungen" v-scrollanimation>
+                <div><img src="../assets/serviceIcon.png" alt=""></div>
+                <div><h2>Serviceleistungen:</h2></div>
+            </div>
+            <!--Service Leistungen Text-->
+                <div class="service-text">
+                    <p v-scrollanimation>
+                        <strong>
+                            Zu unseren Servieleistungen gehört unter anderem die <span> häusliche
+                            Krankenpflege nach §37 SGB V  </span> als Leistung der gesetzlichen Krankenversicherung,
+                            welche unter anderem die <span>Medikamentengabe</span>, den <span>Verbandswechsel</span>  und <span>Injektionen</span> beinhaltet.
+                        </strong>
+                    </p>
+                </div>
+            </div><!--Ende service Kontainer-->
+        </div><!--Ende Intensiv-servie Leistungen Kontainer-->
+                    <!--Grundpflege Balken-->
+                            <div class="balken" >
+                                <div class="balken-text" v-slideInAnimation> 
+                                    <h3>Welche Leistungen zählen zur Grundpflege?</h3>
+                                    <p>alle Leistungen, die die Körperpflege, Ernährung und Mobilität betreffen,
+                                        fallen unter die Grundpflege.
+                                    </p>
+                                </div>
+                                <div class="balken-list" v-scrollanimation>
+                                    <h1>Grundpflege Übersicht: </h1><br>
+                                        <ul> <li>Körperpflege</li></ul>
+                                        <ul> <li>Hilfe beim An - und Ausziehen</li></ul>
+                                        <ul> <li>Hilfe beim Essen und Trinken</li> </ul>
+                                        <ul> <li>Bewegungshilfe</li></ul>
+                                        <ul><li>Hauswirtschaftliche Aufgaben</li></ul>
+                                    </div>
+                                </div>
+
+
+            <div class="balken" >
+                <div class="balken-text" v-slideInAnimation> 
+                    <h3>aus welchen medizinischen Maßnahmen setzt sich unsere Behandlungspflege zusammen?</h3>
+                </div>
+                    <div class="balken-list" v-scrollanimation>
+                        <h1>medizinische Maßnahmen Übersicht: </h1><br>
+                        <ul> <li>Verabreichen von Medikamenten</li></ul>
+                        <ul> <li>Injektionen</li></ul>
+                        <ul> <li>Blutdruck & - zucker Kontrolle</li></ul>
+                        <ul> <li>Wundversorgung</li></ul>
+                    </div>
+                </div>
+                  
+            <!--Intensivpflege Balken-->
+            <div class="balken" >
+                <div class="balken-text" v-slideInAnimation> 
+                    <h3>Welche Leistungen zählen zur Intensivpflege?</h3>
+                    <p>ein wichtiger Bestandteil unserer Leistungen ist die Intensivpflege.</p>
+                </div>
+                    <div class="balken-list" v-scrollanimation>
+                        <h1>Intensivpflege Übersicht: </h1><br>
+                        <ul> <li>Überwachung & Vitalisierung</li></ul>
+                        <ul> <li>Lagerungsbehandlung</li></ul>
+                        <ul> <li>Mobilisation</li></ul>
+                    </div>
+                </div>
+            
+            <!--Service Balken-->
+            <div class="balken" >
+                    <div class="balken-text" v-slideInAnimation> 
+                        <h3>Welche Serviceleistungen stehen Ihnen zur Verfügung?</h3>
+                            <p>Die Sirius Pflegedienst GmbH stellt Ihnen ein attraktives
+                            Paket an Serviceleistungen zur Verfügung.
+                            </p>
+                    </div>
+                        <div class="balken-list" v-scrollanimation>
+                            <h1>Serviceleistungen Übersicht: </h1><br>
+                                <ul> <li>Pflegehilfe</li></ul>
+                                <ul> <li>Begleitung bei Spaziergängen</li></ul>
+                                <ul> <li>Pflegeberatung nach §37 Abs. 3 SGB XI</li> </ul>
+                                <ul> <li>häusliche Pflege nach §37 SGB V</li> </ul>
+                            </div>
+                        </div>
+            </div>
 </template>
 
 <script>
@@ -182,20 +234,33 @@ export default {
 </script>
 
 <style scoped>
-/*Slide animation*/
- @media screen and (min-width: 400px) {
-        .intro-img{
-            display: none;
-        }
-    }
+
+
+
+.before-slide{
+    opacity: 0;
+}
+.slide{
+    opacity: 1;
+    transform: translateX(-100%);
+    animation: slide 2s forwards;
+}
+
+@keyframes slide{
+    100%{transform: translateX(0%);}
+
+}
 
 #startseite{
     /*calling slide animation for this section*/
-    animation: 1s ease-out 0s 1 slide;
+    /*animation: 1s ease-out 0s 1 slide;*/
     margin-bottom: 10%;
+    font-family: sans-serif;
 }
+
+                                /*INTRO-FORM*/
+
 .intro-form{
-    font-family: verdana;
     color: rgb(70,130,180);
     width: 30em;
     height: 25em;
@@ -214,26 +279,28 @@ export default {
     margin-top: 3em;
     margin-left: 2em;
     padding: 3px;
-    font-size: 18px;
-    font-family: verdana;
+    font-size: 22px;
+    font-family: sans-serif;
     width: 20em;
 }
 .form-text-2{
     display:flex;
-    font-size: 18px;
-    font-family: verdana;
+    font-size: 22px;
+    font-family: sans-serif;
+    width: 10em;
 }
 .form-text-3{
-    margin-left: 3.5em;
-    margin-top: 3em;
-    width: 90%;
+    margin-left: 1.5em;
+    margin-top: 7em;
+    width: 70%;
     height: 40px;
     border-radius: 3px;
-    border: 1px solid black;
     background: rgb(70,130,180);
     color: white;
     text-align: center;
-    padding-bottom:8px;
+    padding-bottom:13px;
+    position: absolute;
+
 }
 
 
@@ -251,21 +318,31 @@ export default {
     width: 99.5%;
     
 }
+
+
+                            /*Text Inhalt der Seite*/
+
+
 .inhalt{
-   margin-right: 10%;
+    margin-right: 10%;
     margin-top: 10%;
     margin-left: 10%;
 }
-/*Überschrift*/
+
+                                /*TITEL SCHRIFTFARBE*/
+
+                            
 .titel{
     color: rgb(70,130,180);
 }
-/*Paragraph text*/
+
+
+                                    /*INTRO TEXT*/
+
+
 .intro-text{
     display:flex;
-    font-family: verdana;
-    font-weight: 600;
-    font-size: 14px;
+    font-size: 18px;
     outline-color: initial;
     color: grey;
     text-align: left;
@@ -276,17 +353,24 @@ export default {
     
 }
 .intro-list{
+    font-weight: 600;
     margin-top: 2em;
 }
+
+                        /*TEXT DIVS DER LEISTUNGEN*/
+
 .txt{
-    width: 40em;
+    width: 30em;
 }
 
+                            /*GRUNDPFLEGE*/
+
 .grundpflege{
+    margin-top: 5%;
+    margin-left: 10%;
     display: flex;
     color: rgb(70,130,180);
    
-    
 }
 img{
     margin-top: 1em;
@@ -295,50 +379,101 @@ img{
 
 .grundpflege-text{
     display:flex;
-    flex-wrap: wrap;
-    font-family: verdana;
-    font-weight: 500;
-    font-size: 14px;
+    font-size: 18px;
     color:grey;
     line-height: 25px;
     justify-content: space-between;
+    margin-left: 10%;
+    margin-right: 10%;
 
+}
+.balken{
+    display: flex;
+    margin-top: 5em;
+}
+.balken-text p{
+    margin-left: 18em;
+    font-size: 17px;
+    color: #fff;
+}
+.balken-text{
+    background: rgb(70,130,180);
+    width: 45em;
+    color: #fff;
+    
+}
+.balken-text h3{
+    margin-left: 12em;
+    margin-top: 3em;
+    font-size: 25px;
+}
+.balken-list{
+    font-weight: 600;
+    width: 23em;
+    margin-left: 15em;
+    color: grey;
+}
+
+.balken-list h1{
+    color: rgb(70,130,180);
 }
 
 span{
     color: rgb(70,130,180);
 }
+
+
+                            /*Intensivpflege && service Leistungen*/
+
+.intensiv-service-information{
+    display: flex;
+    margin-left: 10%;
+    margin-right: 10%;
+    justify-content: space-between;
+  
+}
+
+
 .intensiv-pflege{
     display:flex;
     color: rgb(70,130,180);
-    margin-top: 2em;
+    margin-top: 3em;
+
     
 }
 .intensivpflege-text{
     display:flex;
     flex-wrap: wrap;
-    font-family: verdana;
     font-weight: 600;
-    font-size: 14px;
+    font-size: 18px;
     outline-color: initial;
     color: grey;
-    justify-content: space-between;
     line-height: 25px;
-    padding: 10px;
+
 }
 .intensiv-list{
     margin-top: 2em;
 }
 
+
+                            /*Service Leistungen*/
+
 .service-leistungen{
     display:flex;
     color: rgb(70,130,180);
+    margin-top: 3em;
+
 }
 .service-text{
-    font-family: verdana;
-    font-weight: 500;
-    font-size: 14px;
-    color:grey;
+    display:flex;
+    flex-wrap: wrap;
+    font-weight: 600;
+    font-size: 18px;
+    outline-color: initial;
+    color: grey;
+    line-height: 25px;
+    width: 30em;
+    
 }
 
 </style>
